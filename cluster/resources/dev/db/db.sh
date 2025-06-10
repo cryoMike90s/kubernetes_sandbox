f61a3a77e4ae30dd2f55f7a926a2b776ffea570d
miskers <miskers@protonmail.com>
2025-06-10 09:40:49 +0000

    Service for NFS

--- cluster/resources/dev/db/db.sh
+++ cluster/resources/dev/db/db.sh
@@ -1,11 +1,11 @@
 kubectl apply -f namespace.yaml
-kubectl apply -f pV.yaml
-
+#kubectl apply -f StorageClass.yaml
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
 helm upgrade --install \
   -n postgres \
   postgres bitnami/postgresql \
   --set auth.postgresPassword=haslo \
+  --set global.defaultStorageClass=nfs-csi-db \
   --version 16.5.2 \
   --values values.yaml
--- cluster/resources/dev/nfs-provider/nfs-provider.sh
+++ cluster/resources/dev/nfs-provider/nfs-provider.sh
@@ -1,2 +1,4 @@
 helm repo add kubeblocks https://apecloud.github.io/helm-charts
-helm install my-csi-driver-nfs kubeblocks/csi-driver-nfs --namespace kube-system --version 4.10.0
+helm install my-csi-driver-nfs kubeblocks/csi-driver-nfs --namespace kube-system --set controller.runOnControlPlane=true --version 4.10.0
+
+# helm delete my-csi-driver-nfs -n kube-system
