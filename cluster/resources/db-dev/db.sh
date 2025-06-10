25ce7bd8acba48653277995a3ac0aece7cc28b0d
miskers <miskers@protonmail.com>
2025-06-10 10:30:24 +0000

    Cleanups
    - Delete obsolete resources
    - Move resources to directories

--- cluster/resources/db-dev/db.sh
+++ cluster/resources/db-dev/db.sh
@@ -1,11 +1,10 @@
-kubectl apply -f namespace.yaml
-#kubectl apply -f StorageClass.yaml
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
 helm upgrade --install \
-  -n postgres \
+  -n db-dev \
   postgres bitnami/postgresql \
   --set auth.postgresPassword=haslo \
   --set global.defaultStorageClass=nfs-csi-db \
   --version 16.5.2 \
-  --values values.yaml
+  --values values.yaml \
+  --create-namespace
