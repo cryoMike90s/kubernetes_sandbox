3996dc09ca411af8f8322fb69e7c23aa12f1e89f
miskers <miskers@protonmail.com>
2025-06-10 11:28:39 +0000

    Development
    - add global prefix for global resource (or resources that exists within
    kube-system)
    - restructure

--- cluster/resources/db/db.sh
+++ cluster/resources/db/db.sh
@@ -1,10 +1,10 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
 helm upgrade --install \
-  -n db-dev \
+  -n db \
   postgres bitnami/postgresql \
   --set auth.postgresPassword=haslo \
-  --set global.defaultStorageClass=nfs-csi-db-dev \
+  --set global.defaultStorageClass=nfs-csi-db \
   --version 16.5.2 \
   --values values.yaml \
   --create-namespace
