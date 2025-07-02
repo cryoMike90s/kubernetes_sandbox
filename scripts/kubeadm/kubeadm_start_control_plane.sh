177a14fb05224cf04c14bd8b942f65e293023803
miskers <miskers@protonmail.com>
2025-07-02 10:21:12 +0000

    sandbox
    - fix typos
    - fix cp name

--- scripts/kubeadm/kubeadm_start_control_plane.sh
+++ scripts/kubeadm/kubeadm_start_control_plane.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-kubeadm init --config ./configs/kubeconfig-control-plane.yaml
+kubeadm init --config ../../configs/kubeconfig-control-plane.yaml
 
 # Project Calico
 #kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
